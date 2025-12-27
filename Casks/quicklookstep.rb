15965082a85f1ec7a796031c0111ccad8ff40d7b
John Boiles <johnaboiles@gmail.com>
2025-12-27 15:36:31 -0800

    Pin quicklookstep to 1.5

--- Casks/quicklookstep.rb
+++ Casks/quicklookstep.rb
@@ -1,8 +1,8 @@
 cask "quicklookstep" do
-  version "latest"
-  sha256 :no_check
+  version "1.5"
+  sha256 "4ded6e7215c16424844e475521c6524ad548dd90fd69428f17177612165ebebd"
 
-  url "https://github.com/johnboiles/quick-look-step/releases/latest/download/QuickLookStep.zip"
+  url "https://github.com/johnboiles/quick-look-step/releases/download/release/v#{version}/QuickLookStep.zip"
   name "QuickLookStep"
   desc "Fast Quick Look previews and Finder thumbnails for STEP files"
   homepage "https://github.com/johnboiles/quick-look-step"
